2c150e83b5d89703baedbcc2624ac8d27935c2e5
daniel <daniel.hartono@chilindo.com>
2018-08-03 15:50:45 +0700

    client: bootstrap and router

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "daniel-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://216el5o6xc.execute-api.ap-southeast-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_WWVBjUDYb",
+    APP_CLIENT_ID: "2ibeia5mqt7t783sd88sv9h74n",
+    IDENTITY_POOL_ID: "ap-southeast-1:571919a3-49e1-4092-9b0e-2d4faa754ed6"
+  }
+};
