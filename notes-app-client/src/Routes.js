0c4a033421ed518a6ea6504210a4070ffbf76dcf
daniel <daniel.hartono@chilindo.com>
2018-08-06 12:46:15 +0700

    client: Create note, login redirect

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -3,12 +3,20 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import NewNote from "./containers/NewNote";
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import Notes from "./containers/Notes";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
