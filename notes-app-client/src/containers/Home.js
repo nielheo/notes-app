5981c68d20ffa4816bf66e9e63d9bf84435eecea
daniel <daniel.hartono@chilindo.com>
2018-08-06 17:01:04 +0700

    update deploy script

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
 import "./Home.css";
 
@@ -45,9 +46,17 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+      <h1>Scratch</h1>
+      <p>A simple note taking app</p>
+      <div>
+        <Link to="/login" className="btn btn-info btn-lg">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
       </div>
+    </div>
     );
   }
 
